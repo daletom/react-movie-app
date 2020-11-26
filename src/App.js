a276a73a3186e0458a18c710953792d6dc1c17f4
JamieDawson <jamierobertdawson@gmail.com>
2020-11-26 15:48:57 -0800

    finalized this app

--- src/App.js
+++ src/App.js
@@ -3,38 +3,52 @@ import Movie from './components/Movie';
 import './App.css';
 require('dotenv').config();
 
-const FEATURED = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_API_SECRET}`;
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_SECRET}`;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_API_SECRET}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_SECRET}&query=`;
 
 function App() {
 	const [movies, setMovies] = useState([]);
 	const [searchTerm, setSearchTerm] = useState('');
 
 	useEffect(() => {
-		fetch(FEATURED)
+		getMovies(FEATURED_API);
+	}, []);
+
+	const getMovies = (API) => {
+		fetch(API)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
 				setMovies(data.results);
 			});
-	}, []);
+	};
 
 	const handleOnSubmit = (e) => {
 		e.preventDefault();
+		if (searchTerm) {
+			getMovies(SEARCH_API + searchTerm);
+			setSearchTerm('');
+		}
+	};
+
+	const handleOnChange = (e) => {
+		setSearchTerm(e.target.value);
 	};
 
 	return (
 		<>
-			<form onSubmit={handleOnSubmit}>
-				<header>
+			<header>
+				<div className='header-text'>Movie Search build in React</div>
+				<form onSubmit={handleOnSubmit}>
 					<input
 						className='search'
 						type='search'
-						placeholder='serach...'
+						placeholder='Search movie title'
 						value={searchTerm}
+						onChange={handleOnChange}
 					/>
-				</header>
-			</form>
+				</form>
+			</header>
+
 			<div className='movie-container'>
 				{movies.length > 0 &&
 					movies.map((movie) => <Movie key={movie.id} {...movie} />)}
@@ -47,3 +61,5 @@ export default App;
 
 // https://developers.themoviedb.org/3/getting-started/introduction
 // set up api
+
+//30:25
