3e3213cb3ff3a97750c39d4c0592ea3209f1371b
JamieDawson <jamierobertdawson@gmail.com>
2021-08-26 21:31:39 -0700

    Added basic image editing with API call. Images are now 300 by 450. I will not test this using react-imgix

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Imgix from 'react-imgix';
 
-const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+const IMG_API =
+	'https://jamie-dawson.imgix.net/https://image.tmdb.org/t/p/w1280';
+
+const IMGIX_PARAMS = '?w=300&h=450';
 
 const setVoteClass = (vote) => {
 	if (vote >= 8) {
@@ -12,12 +16,16 @@ const setVoteClass = (vote) => {
 	}
 };
 
+let pass = 'can I pass this';
+
 const Movie = ({title, poster_path, overview, vote_average}) => (
 	<div className='movie'>
+		<Imgix src='https://assets.imgix.net/examples/pione.jpg' sizes='100vw' />
+
 		<img
 			src={
 				poster_path
-					? IMG_API + poster_path
+					? IMG_API + poster_path + IMGIX_PARAMS
 					: 'https://images.unsplash.com/photo-1538152911114-73f1aa6d6128?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80'
 			}
 			alt={title}
@@ -28,7 +36,6 @@ const Movie = ({title, poster_path, overview, vote_average}) => (
 				{vote_average}
 			</span>
 		</div>
-
 		<div className='movie-over'>
 			<h2>Overview:</h2>
 			<p>{overview}</p>
