b7c2b65b849c6e0a492421b5487d9045255721d7
daletom <tom@imgix.com>
2021-09-16 14:47:37 -0700

    update imgix react

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Imgix from 'react-imgix';
 
 const setVoteClass = (vote) => {
 	if (vote >= 8) {
@@ -12,37 +13,13 @@ const setVoteClass = (vote) => {
 
 const Movie = ({title, poster_path, overview, vote_average, mono}) => (
 	<div className='movie'>
-		<img
-			srcSet={
-				'https://jamie-dawson.imgix.net/image.tmdb.org/t/p/w1280' +
-				poster_path +
-				'?w=300&h=450&dpr=1&txt=dpr1&q=75&auto=format' +
-				mono +
-				' 1x,' +
-				'https://jamie-dawson.imgix.net/image.tmdb.org/t/p/w1280' +
-				poster_path +
-				'?w=300&h=450&dpr=2&txt=dpr2&q=75&auto=format' +
-				mono +
-				' 2x,' +
-				'https://jamie-dawson.imgix.net/image.tmdb.org/t/p/w1280' +
-				poster_path +
-				'?w=300&h=450&dpr=3&txt=dpr3&q=75&auto=format' +
-				mono +
-				' 3x,' +
-				'https://jamie-dawson.imgix.net/image.tmdb.org/t/p/w1280' +
-				poster_path +
-				'?w=300&h=450&dpr=4&txt=dpr4&q=75&auto=format' +
-				mono +
-				' 4x,' +
-				'https://jamie-dawson.imgix.net/image.tmdb.org/t/p/w1280' +
-				poster_path +
-				'?w=300&h=450&dpr=5&txt=dpr5&q=75&auto=format' +
-				mono +
-				' 5x'
-			}
+		<Imgix
 			src={
-				'https://jamie-dawson.imgix.net/image.tmdb.org/t/p/w1280' + poster_path
+				'https://jamie-dawson.imgix.net/t/p/w1280' + poster_path
 			}
+			width={300}
+			height={450}
+			imgixParams={{ fit: "crop", auto: "format,compress"}}
 			alt={title}
 		/>
 		<div className='movie-info'>
